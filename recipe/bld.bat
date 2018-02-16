ccdcddf491ed30b9300c0fa36bd52cf8090cdbbf
Brian Rose <brose@albany.edu>
2018-02-16 06:53:43 -0500

    Update recipe for climlab v0.6.3 (#11)
    * Try again building for win/py36
    
    * MNT: Re-rendered with conda-smithy 2.4.4
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Use the full fast test suite
    
    * Use flang to build for win/py3k
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Skip build on for win/py2k.
    
    Not sure why this is failing since it used to work fine, but win/py3k is higher priority.
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Only build for win/py36 -- for now at least
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Also exclude osx/py35 because tests fail with numpy 1.14 for some strange reason
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * We lost win/py36 somehow. Try to render again.
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Update recipe for climlab v0.6.3.dev4
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * Update recipe for climlab v0.6.3.dev5, try build win32/py2k
    
    * Update recipe for climlab v0.6.3.dev6
    
    * Update recipe for climlab v0.6.3.dev7
    
    * Update recipe for climlab v0.6.3
    
    * MNT: Re-rendered with conda-smithy 2.4.5

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,12 @@
-set CFG=%USERPROFILE%\pydistutils.cfg
-echo [config] > "%CFG%"
-echo compiler=mingw32 >> "%CFG%"
-echo [build] >> "%CFG%"
-echo compiler=mingw32 >> "%CFG%"
-echo [build_ext] >> "%CFG%"
-echo compiler=mingw32 >> "%CFG%"
+if "%PY3K"=="0" (
+  set CFG=%USERPROFILE%\pydistutils.cfg
+  echo [config] > "%CFG%"
+  echo compiler=mingw32 >> "%CFG%"
+  echo [build] >> "%CFG%"
+  echo compiler=mingw32 >> "%CFG%"
+  echo [build_ext] >> "%CFG%"
+  echo compiler=mingw32 >> "%CFG%"
+)
 
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
