d314474a333c3a9df9c55b88b0e365c63f7c908a
Brian Rose <brose@albany.edu>
2022-02-02 15:14:40 -0500

    New bld.bat script following fastscapelib-f2py-feedstock

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,11 @@
-if "%PY3K"=="0" (
-  set CFG=%USERPROFILE%\pydistutils.cfg
-  echo [config] > "%CFG%"
-  echo compiler=mingw32 >> "%CFG%"
-  echo [build] >> "%CFG%"
-  echo compiler=mingw32 >> "%CFG%"
-  echo [build_ext] >> "%CFG%"
-  echo compiler=mingw32 >> "%CFG%"
-)
+@echo on
 
-REM remove chocolatey gfortran
-if "%CI%" == "azure" (
-  rm "C:\ProgramData\Chocolatey\bin\gfortran.exe"
-)
+dir %RECIPE_DIR%
+
+echo %LIB%
+
+set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 
 "%PYTHON%" -m pip install . --no-deps -vv
+
 if errorlevel 1 exit 1
